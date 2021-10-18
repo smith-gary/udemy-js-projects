388a6bdf473ad04aac267f777498117db3495145
gary_s <smithgd34@gmail.com>
2021-10-18 08:30:38 -0500

    Showing the difference of the for of loop and the forEach loop.

--- Arrays/script.js
+++ Arrays/script.js
@@ -71,10 +71,10 @@ const currencies = new Map([
   ['GBP', 'Pound sterling'],
 ]);
 
-const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+// const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
 /////////////////////////////////////////////////
-
+/*
 // SLICE
 let arr = ['a', 'b', 'c', 'd', 'e'];
 console.log(arr.slice(2)); // copy of array with the extracted parts
@@ -105,3 +105,27 @@ console.log([...arr, ...arr2]);
 // JOIN
 
 console.log(letters.join(' - '));
+*/
+
+const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+
+// for (const movement of movements) {
+for (const [i, movement] of movements.entries()) {
+  if(movement > 0) {
+    console.log(`Movement ${i + 1}: You deposited ${movement}.`);
+  } else {
+    console.log(`Movement ${i + 1}: You withdrew ${Math.abs(movement)}`)
+  };
+};
+
+console.log('-------FOR EACH----------');
+
+movements.forEach(function(mov, i, arr) {
+  if(mov > 0) {
+    console.log(`Movement ${i + 1}: You deposited ${mov}.`);
+  } else {
+    console.log(`Movement ${i + 1}: You withdrew ${Math.abs(mov)}`)
+  };
+});
+
+// You cannot break out of a forEach loop
